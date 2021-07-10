bb79031ab404f78779b74fcc35358c37443f8027
WillYip716 <william.yip.716@gmail.com>
2021-07-09 21:16:19 -0700

    fbball calculator front only

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Home from './components/home';
+import About from './components/about';
+import NavbarComp from './components/navbar';
+import Roster from './components/rosters';
+import Team from './components/team';
+import LeagueCompiler from './components/leaguecompiler';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Rankings from './components/rankings';
+import Footer from './components/footer';
+import Trade from './components/trade'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavbarComp/>
+        <div className="main">
+          <Route path="/" exact component={Home} />
+          <Route path="/rankings" exact component={Rankings} />
+          <Route path="/rosters" exact component={Roster} />
+          <Route path="/team/:id" exact component={Team} />
+          <Route path="/about" exact component={About} />
+          <Route path="/compile" exact component={LeagueCompiler} />
+          <Route path="/trade" exact component={Trade} />
+        </div>
+      <Footer/>
     </div>
   );
 }
